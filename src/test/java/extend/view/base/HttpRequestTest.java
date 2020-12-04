9c1df26fd9bcc61b19b649c3d90f3ca49668d950
root <raise.isayan@gmail.com>
2020-12-04 21:31:00 +0900

    fix contentType

--- src/test/java/extend/view/base/HttpRequestTest.java
+++ src/test/java/extend/view/base/HttpRequestTest.java
@@ -4,6 +4,8 @@ import java.nio.charset.StandardCharsets;
 import java.text.ParseException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -51,6 +53,32 @@ public class HttpRequestTest {
         "Connection: close\r\n" +
         "\r\n" +
         "text=%82%A0%82%A2%82%A4%82%A6%82%A8&OS=win&submit=%91%97%90M\r\n";    
+
+    private final static String REQ_MESSAGE_URLENCODE2 =
+        "POST /jvuln/CSRFVuln HTTP/1.1\n" +
+        "Host: 192.168.0.1\r\n" +
+        "Content-Length: 35\r\n" +
+        "Content-Type: application/x-www-form-urlencoded\r\n" +
+        "test: test\r\n" +
+        "Accept-Encoding: gzip, deflate\r\n" +
+        "Accept-Language: ja,en-US;q=0.9,en;q=0.8\r\n" +
+        "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36\r\n" +
+        "Connection: close\r\n" +
+        "\r\n" +
+        "comment=%E3%83%86%E3%82%B9%E3%83%88";
+
+    private final static String REQ_MESSAGE_JSON =
+        "POST /jvuln/CSRFVuln HTTP/1.1\n" +
+        "Host: 192.168.0.1\r\n" +
+        "Content-Length: 35\r\n" +
+        "Content-Type: application/json; charset=UTF-8\r\n" +
+        "test: test\r\n" +
+        "Accept-Encoding: gzip, deflate\r\n" +
+        "Accept-Language: ja,en-US;q=0.9,en;q=0.8\r\n" +
+        "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36\r\n" +
+        "Connection: close\r\n" +
+        "\r\n" +
+        "{\"comment\":\"テスト\"}";
 
     private final static String REQ_MESSAGE_MULTIPART =
         "POST /cgi-bin/sendto.cgi?mode=sendto HTTP/1.1\r\n" +
@@ -110,10 +138,33 @@ public class HttpRequestTest {
     public void testGetEncType() {
         try {
             System.out.println("getEncType");
-            HttpRequest req = HttpRequest.parseHttpRequest(REQ_MESSAGE_URLENCODE);
-            String result = req.getEnctype();
-            String expResult = "application/x-www-form-urlencoded";
-            assertEquals(expResult, result);            
+            {
+                HttpRequest req = HttpRequest.parseHttpRequest(REQ_MESSAGE_URLENCODE);
+                String result = req.getEnctype();
+                String expResult = "application/x-www-form-urlencoded";
+                assertEquals(expResult, result);
+            }
+            {
+                HttpRequest req = HttpRequest.parseHttpRequest(REQ_MESSAGE_URLENCODE2);
+                String result = req.getEnctype();
+                String expResult = "application/x-www-form-urlencoded";
+                assertEquals(expResult, result);
+            }
+            {
+                HttpRequest req = HttpRequest.parseHttpRequest(REQ_MESSAGE_JSON);
+                String result = req.getEnctype();
+                String expResult = "application/json";
+                assertEquals(expResult, result);
+            }
+            {
+                final Pattern CONTENT_TYPE = Pattern.compile("^Content-Type:\\s*([^;]+?)(?:;\\s*charset=[\"\']?([\\w_-]+)[\"\']?)?\\s*$", Pattern.MULTILINE);
+                Matcher m = CONTENT_TYPE.matcher(REQ_MESSAGE_URLENCODE2);
+                if (m.find()) {
+                    System.out.println("grp1" + m.group(1));
+                    System.out.println("grp2" + m.group(2));
+                }
+            }
+
         } catch (ParseException ex) {
             Logger.getLogger(HttpRequestTest.class.getName()).log(Level.SEVERE, null, ex);
         }
